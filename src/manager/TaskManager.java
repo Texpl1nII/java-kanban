20a615f33b744e973348ca9843794bd54f7b0245
Texpl1n <Texp1ln@yandex.ru>
2025-04-27 23:34:43 +0500

    Снова исправляем

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -2,26 +2,42 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getAllTasks();
+
     void clearTasks();
+
     Task getTask(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     List<Epic> getAllEpics();
+
     void clearEpics();
+
     Epic getEpic(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
     List<Subtask> getAllSubtasks();
+
     void clearSubtasks();
+
     Subtask getSubtask(int id);
+
     Subtask createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
 
     List<Subtask> getEpicSubtasks(int epicId);
+
     List<Task> getHistory();
 }
--- src/model/Task.java
+++ src/model/Task.java
@@ -10,14 +10,37 @@ public class Task {
         this.status = Status.NEW;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public Status getStatus() { return status; }
-    public void setStatus(Status status) { this.status = status; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
     @Override
     public boolean equals(Object o) {
