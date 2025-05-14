74bacfa631d75b40540b2d14ae42d4b5c698185b
Texpl1n <Texp1ln@yandex.ru>
2025-05-15 00:12:13 +0500

    Выполняю задание ФЗ7

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -4,10 +4,20 @@ import manager.TaskManager;
 import manager.InMemoryTaskManager;
 import manager.HistoryManager;
 import manager.InMemoryHistoryManager;
+import manager.FileBackedTaskManager;
+
+
+import java.io.File;
+import java.io.IOException;
 
 public class Managers {
     public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+        try {
+            File file = File.createTempFile("tasks", ".csv");
+            return new FileBackedTaskManager(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Error creating temp file for FileBackedTaskManager", e);
+        }
     }
 
     public static HistoryManager getDefaultHistory() {
